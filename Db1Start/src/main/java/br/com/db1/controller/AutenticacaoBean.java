e6d1182fa3899682786705626c0db669604f3068
linjardi <daniel_linjardi@hotmail.com>
2017-10-21 21:57:47 -0200

    melhorias no css

--- Db1Start/src/main/java/br/com/db1/controller/AutenticacaoBean.java
+++ Db1Start/src/main/java/br/com/db1/controller/AutenticacaoBean.java
@@ -23,9 +23,11 @@ public class AutenticacaoBean {
 			ExternalContext ec = fc.getExternalContext();
 			HttpSession session = (HttpSession) ec.getSession(false);
 			session.setAttribute("usuario", this.usuario);
-			return "/logado/home";
+			return "rh";
 
-		} else {
+		}
+		
+		else {
 			FacesMessage fm = new FacesMessage("Usuário e/ou senha inválidos");
 			fm.setSeverity(FacesMessage.SEVERITY_ERROR);
 		fc.addMessage(null, fm);
