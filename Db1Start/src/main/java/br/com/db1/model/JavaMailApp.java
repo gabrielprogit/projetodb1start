5c78cea2e0125b6cfa87bbe92be6330195ca1ce6
hiagoluvi <hiagoluvizutto@gmail.com>
2017-10-23 22:58:14 -0200

    Email e tela Fusca

--- Db1Start/src/main/java/br/com/db1/model/JavaMailApp.java
+++ Db1Start/src/main/java/br/com/db1/model/JavaMailApp.java
@@ -8,44 +8,35 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import org.omg.CORBA.Environment;
+
 public class JavaMailApp {
-	static Session session; //declarar como um atributo de classe e static(global)
-	public static void main(String[] args) {  
-        Properties props = new Properties();  
-        /** Parâmetros de conexão com servidor Hotmail */  
-        props.put("mail.transport.protocol", "smtp");  
-        props.put("mail.smtp.host", "smtp.live.com");  
-        props.put("mail.smtp.socketFactory.port", "587");  
-        props.put("mail.smtp.socketFactory.fallback", "false");  
-        props.put("mail.smtp.starttls.enable", "true");  
-        props.put("mail.smtp.auth", "true");  
-        props.put("mail.smtp.port", "587");  
-       session = Session.getDefaultInstance(props,  
-                    new javax.mail.Authenticator() {  
-                         protected PasswordAuthentication getPasswordAuthentication()   
-                         {  
-                               return new PasswordAuthentication("sysoutech@hotmail.com", "Senhafacil123");  
-                         }  
-                    });  
-        /** Ativa Debug para sessão */  
-        session.setDebug(true);  
-        enviaHotmail(); //chama o método que é declarado como static nesse contexto
-  }  
-	public static void enviaHotmail() {
-		try {  
-            Message message = new MimeMessage(session);  
-            message.setFrom(new InternetAddress("sysoutech@hotmail.com")); //Remetente  
-            message.setRecipients(Message.RecipientType.TO,   
-                              InternetAddress.parse("geovanebsu8@hotmail.com")); //Destinatário(s)  
-            message.setSubject("Testando Protocolo de Email");//Assunto  
-            message.setText("As mina pira no Tester" +
-            		"\n Hahahaha");  
-            /**Método para enviar a mensagem criada*/  
-            Transport.send(message);
-            System.out.println("Feito!!!");  
-       } catch (MessagingException e) {  
-            throw new RuntimeException(e);  
-      }  
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.host", "smtp.live.com");
+		props.put("mail.smtp.socketFactory.port", "587");
+		props.put("mail.smtp.socketFactory.fallback", "false");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("-----------", "---------------");
+			}
+		});
+		session.setDebug(true);
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("hiago_luvizutto@hotmail.com")); 
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(""));
+			message.setSubject("Enviando email com JavaMail");
+			message.setText("SYSOUT TECH É SHOW!!!!");
+			Transport.send(message);
+			System.out.println("Feito!!!");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
+
 }
